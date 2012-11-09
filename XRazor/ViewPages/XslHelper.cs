54112d862fa8693018f91131f0bed90c5eb93de5
Sergey Zigachev <s.zi@outlook.com>
2012-11-09 12:54:44 +0400

    Added replacement class for WebViewPage

---
+++ XRazor/ViewPages/XslHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace XRazor.ViewPages
+{
+    public class XslHelper<TModel> where TModel : class
+    {
+        private ViewContext _viewContext;
+        private XsltWebViewPage<TModel> _xsltWebViewPage;
+
+        public XslHelper(ViewContext viewContext, XsltWebViewPage<TModel> xsltWebViewPage)
+        {
+            _viewContext = viewContext;
+            _xsltWebViewPage = xsltWebViewPage;
+        }
+    }
+}
---
+++ XRazor/ViewPages/XsltWebViewPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XRazor.ViewPages
+{
+    public abstract class XsltWebViewPage<TModel> : WebViewPage<TModel> where TModel : class 
+    {
+        public XslHelper<TModel> Xsl { get; set; }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+            Xsl = new XslHelper<TModel>(base.ViewContext, this);
+        }
+    }
+}
