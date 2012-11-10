653a1aed0e003496f2ffae1cedc05884c53b77e5
Sergey Zigachev <s.zi@outlook.com>
2012-11-10 09:25:43 +0400

    Added TemplateRule object

---
+++ XRazor/ViewPages/TemplateRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.XPath;
+using System.Web.WebPages;
+
+namespace XRazor.ViewPages
+{
+	public class TemplateRule
+	{
+		public XPathExpression XPath { get; set; }
+		public Func<dynamic, HelperResult> Template{ get; set; }
+		public decimal Priority { get; set; }
+
+		public TemplateRule(string xpath, Func<dynamic, HelperResult> template)
+		{
+			XPath = XPathExpression.Compile(xpath);
+			Template = template;
+		}
+	}
+}
